5e9b5b956e85d313ba64f19068cc5cccb2dc2a4e
DemidCranel <demidcraft777@gmail.com>
2025-01-25 11:44:02 +0300

    Фикс + новая механика
    Пофикшен баг с пробелом и шифтом | Теперь таргетов есть 2 вида: 1 вид - 1 хп, оранжевый цвет, большой размер, большая скорость, 2 вид - 2 хп, зеленый цвет при фулл хп, желтый цвет при половине хп, малый размер, малая скорость. Каждая мишень теперь генерируется рандомного размера.

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 # Отрисовать декорации, сделать счетчик, сделать хп и перезапуск счетчика после падения хп до 0
-# Сделать чтобы у мишеней было по 2 хп и когда у мишени оставалось 1 хп, то она перекрасивалась бы в желтый
-# Переделать список со всеми текущими пулями и мишенями в словарь (для реализации хп у мишеней) и адаптировать это.
-# Либо оставить список, но его содержимое будет словарем со всеми характеристиками
 
 import pygame
 import time
@@ -28,8 +25,8 @@ emulate_config = {
         'size':50,
     },
     'target_settings':{
-        'size':125,
-        'speed':2.75,
+        # 'size':125,
+        'speed':3.25,
         'cooldown':1,
     }
 }
@@ -51,6 +48,13 @@ class Player():
 
 
     def move(self):
+        if self.speed_status != 0 and shift_up and not space_up:
+            self.speed_status = 2
+        elif self.speed_status != 0 and space_up and not shift_up:
+            self.speed_status = 1
+        elif self.speed_status != 0 and space_up and shift_up:
+            self.speed_status = 0
+
         if self.line == "Right":
             if self.speed_status == 0:
                 self.coord[0] += self.speed
@@ -94,25 +98,33 @@ class Bullets():
 
     def new_bullet(self):
         if time.time() - self.time_last_bullet > self.cd_bullet:
-            self.bullets_all.append([player.coord[0], player.coord[1]])
+            self.bullets_all.append(
+                {'coord':[player.coord[0], player.coord[1]],
+                 'damage':emulate_config['player_save']['damage']
+                 })
             self.time_last_bullet = time.time()
             self.actual_targets += 1
 
     def bullet_render(self):
         for bullet in self.bullets_all:
+            bullet = bullet['coord']
             pygame.draw.rect(screen, (255, 55, 55), (bullet[0], bullet[1] - player.size / 2 + self.width, self.width, self.height))
 
     def tick(self):
         if target.actual_targets > 0 and self.actual_targets > 0:
             for index_bullet in range(len(self.bullets_all)):
                 for index_target in range(len(target.all_targets)):
-                    if self.bullets_all[index_bullet][0] > target.all_targets[index_target][0] and self.bullets_all[index_bullet][0] < target.all_targets[index_target][0] + target.size:
-                        if self.bullets_all[index_bullet][1] > target.all_targets[index_target][1] and self.bullets_all[index_bullet][1] < target.all_targets[index_target][1] + target.size:
+                    if self.bullets_all[index_bullet]['coord'][0] > target.all_targets[index_target]['coord'][0] and self.bullets_all[index_bullet]['coord'][0] < target.all_targets[index_target]['coord'][0] + target.all_targets[index_target]['size']:
+                        if self.bullets_all[index_bullet]['coord'][1] > target.all_targets[index_target]['coord'][1] and self.bullets_all[index_bullet]['coord'][1] < target.all_targets[index_target]['coord'][1] + target.all_targets[index_target]['size']:
                             self.delete_bullets.append(index_bullet)
-                            self.delete_targets.append(index_target)
+                            if target.all_targets[index_target]['hp'] - player.damage <= 0:
+                                self.delete_targets.append(index_target)
+                            else:
+                                target.all_targets[index_target]['hp'] -= player.damage
+                                target.all_targets[index_target]['color'] = (204, 255, 0)
         for index in range(len(self.bullets_all)):
-            self.bullets_all[index][1] -= self.speed
-            if self.bullets_all[index][1] + self.width < 0:
+            self.bullets_all[index]['coord'][1] -= self.speed
+            if self.bullets_all[index]['coord'][1] + self.width < 0:
                 self.delete_bullets.append(index)
         for index in self.delete_bullets:
             self.bullets_all.pop(index)
@@ -126,8 +138,8 @@ class Bullets():
 
 class Target():
 
-    def __init__(self, size, speed, cooldown):
-        self.size = size
+    def __init__(self, speed, cooldown):
+        # self.size = size
         self.speed = speed
         self.all_targets = []
         self.actual_targets = 0
@@ -137,15 +149,25 @@ class Target():
 
     def new_target(self):
         if time.time() - self.last_time > self.cooldown: # обавить условие
-            self.all_targets.append([random.randint(self.size, screen_xy[0] - self.size * 2), self.size * -1])
+            if random.randint(1,2) == 1:
+                size_target = random.randint(75,110)
+            else:
+                size_target = random.randint(140, 175)
+            self.all_targets.append({
+                'coord':[random.randint(size_target, screen_xy[0] - size_target * 2), size_target * -1],
+                'hp':2 if size_target < 125 else 1,
+                'size': size_target,
+                'speed':self.speed if size_target > 125 else self.speed - (self.speed / 100) * 50,
+                'color': (247, 143, 57) if size_target > 125 else (0, 255, 9)
+            })
             self.actual_targets += 1
             self.last_time = time.time()
 
     def target_tick(self):
         self.new_target()
         for index in range(len(self.all_targets)):
-            self.all_targets[index][1] += self.speed
-            if self.all_targets[index][1] > screen_xy[1]:
+            self.all_targets[index]['coord'][1] += self.all_targets[index]['speed']
+            if self.all_targets[index]['coord'][1] > screen_xy[1]:
                 self.delete_targets.append(index)
         for index in self.delete_targets:
             self.all_targets.pop(index)
@@ -154,7 +176,7 @@ class Target():
 
     def target_render(self):
         for target in self.all_targets:
-            pygame.draw.rect(screen, (247, 143, 57), (target[0], target[1], self.size, self.size))
+            pygame.draw.rect(screen, target['color'], (target['coord'][0], target['coord'][1], target['size'], target['size']))
 
 
 
@@ -187,14 +209,17 @@ player = Player( # Характеристики игрока
 )
 bullet = Bullets(cd_bullet=emulate_config['player_save']['atack_speed']) # cd_bullet содержит в себе минимальное значение в секундах между выстрелами
 target = Target(
-    size=emulate_config['target_settings']['size'],
+    # size=emulate_config['target_settings']['size'],
     speed=emulate_config['target_settings']['speed'],
     cooldown=emulate_config['target_settings']['cooldown'],
 )
 
 mouse_lkm_down = False
+shift_up = True
+space_up = True
 
 while True:
+
     # Проверка событий №1
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
@@ -207,13 +232,15 @@ while True:
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_LSHIFT:
             player.speed_status = 1
+            shift_up = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             player.speed_status = 2
+            space_up = False
 
         if event.type == pygame.KEYUP and event.key == pygame.K_LSHIFT:
-            player.speed_status = 0
+            shift_up = True
         if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
-            player.speed_status = 0
+            space_up = True
 
     # Проверка событий №2
     keys = pygame.key.get_pressed()
