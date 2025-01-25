1206ba56d3c58282e1560fec62aa606dea7ad4e8
DemidCranel <demidcraft777@gmail.com>
2025-01-25 22:16:54 +0300

    Новые механики
    Теперь у игрока есть хп которое отнимается когда мишень заходит за экран. Так же добавлен счет (по 5 очков за 1 попадание). Когда хп игрока дойдет до нуля оно сбросится до дефолтного а счет обнулится до нуля.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Отрисовать декорации, сделать счетчик, сделать хп и перезапуск счетчика после падения хп до 0
+# Отрисовать декорации
 
 import pygame
 import time
@@ -11,21 +11,21 @@ screen = pygame.display.set_mode(screen_xy)
 clock = pygame.time.Clock()
 bullet_speed = 20
 
+font_text = pygame.font.SysFont('Comic Sans MS', 50)
+
 emulate_config = {
     'player_save':{
-        'hp':3,
+        'hp':5,
         'speed':9,
         'speed_down':3,
         'speed_up':15,
         'damage':1,
         'atack_speed':0.25,
-        'money_multi':1,
         'coord':[500,830],
         'color':[255,255,255],
         'size':50,
     },
     'target_settings':{
-        # 'size':125,
         'speed':3.25,
         'cooldown':1,
     }
@@ -45,6 +45,8 @@ class Player():
         self.speed_down = speed_down
         self.speed_up = speed_up
         self.speed_status = 0 # 0 - обычная скорость, 1 - быстрая скорость, 2 - медленная скорость
+        self.hp = hp
+        self.score = 0
 
 
     def move(self):
@@ -81,6 +83,11 @@ class Player():
     def player_render(self):
         pygame.draw.rect(screen, (255,255,255), (self.coord[0] - self.size/2, self.coord[1] - self.size/2, self.size, self.size)) # 25 вычитаем из-за того что это половина от размера игрока
 
+    def player_tick(self):
+        if self.hp <= 0:
+            self.hp = emulate_config['player_save']['hp']
+            self.score = 0
+
 
 
 class Bullets():
@@ -116,6 +123,7 @@ class Bullets():
                 for index_target in range(len(target.all_targets)):
                     if self.bullets_all[index_bullet]['coord'][0] > target.all_targets[index_target]['coord'][0] and self.bullets_all[index_bullet]['coord'][0] < target.all_targets[index_target]['coord'][0] + target.all_targets[index_target]['size']:
                         if self.bullets_all[index_bullet]['coord'][1] > target.all_targets[index_target]['coord'][1] and self.bullets_all[index_bullet]['coord'][1] < target.all_targets[index_target]['coord'][1] + target.all_targets[index_target]['size']:
+                            player.score += 5
                             self.delete_bullets.append(index_bullet)
                             if target.all_targets[index_target]['hp'] - player.damage <= 0:
                                 self.delete_targets.append(index_target)
@@ -126,9 +134,11 @@ class Bullets():
             self.bullets_all[index]['coord'][1] -= self.speed
             if self.bullets_all[index]['coord'][1] + self.width < 0:
                 self.delete_bullets.append(index)
-        for index in self.delete_bullets:
-            self.bullets_all.pop(index)
-            self.actual_targets -= 1
+        try:
+            for index in self.delete_bullets:
+                self.bullets_all.pop(index)
+                self.actual_targets -= 1
+        except: pass
         for index in self.delete_targets:
             target.all_targets.pop(index)
         self.delete_bullets = []
@@ -139,7 +149,6 @@ class Bullets():
 class Target():
 
     def __init__(self, speed, cooldown):
-        # self.size = size
         self.speed = speed
         self.all_targets = []
         self.actual_targets = 0
@@ -148,7 +157,7 @@ class Target():
         self.last_time = 0
 
     def new_target(self):
-        if time.time() - self.last_time > self.cooldown: # обавить условие
+        if time.time() - self.last_time > self.cooldown:
             if random.randint(1,2) == 1:
                 size_target = random.randint(75,110)
             else:
@@ -158,7 +167,7 @@ class Target():
                 'hp':2 if size_target < 125 else 1,
                 'size': size_target,
                 'speed':self.speed if size_target > 125 else self.speed - (self.speed / 100) * 50,
-                'color': (247, 143, 57) if size_target > 125 else (0, 255, 9)
+                'color': (247, 143, 57) if size_target > 125 else (0, 255, 9),
             })
             self.actual_targets += 1
             self.last_time = time.time()
@@ -169,6 +178,7 @@ class Target():
             self.all_targets[index]['coord'][1] += self.all_targets[index]['speed']
             if self.all_targets[index]['coord'][1] > screen_xy[1]:
                 self.delete_targets.append(index)
+                player.hp -= 1
         for index in self.delete_targets:
             self.all_targets.pop(index)
             self.actual_targets -= 1
@@ -189,8 +199,16 @@ def start_cycle():
     target.target_render() # Отрисовываем таргет
     player.player_render() # Отрисовываем игрока
 
+    # Текст здоровья
+    text_hp = font_text.render(f'Здоровье: {player.hp}', False, (255, 255, 255))
+    screen.blit(text_hp, (50, 50))
+    # Текст счета
+    text_score = font_text.render(f'Счет: {player.score}', False, (255, 255, 255))
+    screen.blit(text_score, (50, 110))
+
     target.target_tick()
     bullet.tick()
+    player.player_tick()
     clock.tick(160) # Ограничение фпс
 
 
@@ -209,7 +227,6 @@ player = Player( # Характеристики игрока
 )
 bullet = Bullets(cd_bullet=emulate_config['player_save']['atack_speed']) # cd_bullet содержит в себе минимальное значение в секундах между выстрелами
 target = Target(
-    # size=emulate_config['target_settings']['size'],
     speed=emulate_config['target_settings']['speed'],
     cooldown=emulate_config['target_settings']['cooldown'],
 )
